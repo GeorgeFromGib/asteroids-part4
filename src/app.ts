25fb65ce22390d5f2149e2aedba3a59c9533ddd9
George Gaskin <george_gaskin@msn.com>
2021-07-03 18:02:06 +0200

    Backup

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,32 @@
 import P5 from "p5";
 
-
 // Creating the sketch itself
-const sketch = (p5: P5) => {
+const sketch = (p5: P5, windowWidth: number, windowHeight: number) => {
+  // The sketch setup method
+  p5.setup = () => {
+    // Creating and positioning the canvas
+    const canvas = p5.createCanvas(windowWidth, windowHeight);
+    canvas.parent("app");
 
-	// The sketch setup method 
-	p5.setup = () => {
-		// Creating and positioning the canvas
-		const canvas = p5.createCanvas(200, 200);
-		canvas.parent("app");
+    // Configuring the canvas
+    p5.background("black");
+  };
 
-		// Configuring the canvas
-		p5.background("black");
-
-	};
-
-	// The sketch draw method
-	p5.draw = () => {
-		// DEMO: Let the circle instances draw themselves
-        p5.fill(255);
-        p5.rect(100, 100, 50, 50);
-	};
+  // The sketch draw method
+  p5.draw = () => {
+    //Draw the spaceship
+    p5.translate(p5.width / 2, p5.height / 2);
+	p5.scale(2);
+	p5.noFill();
+	p5.stroke('white');
+	
+    p5.beginShape();
+    p5.vertex(0, -5);
+    p5.vertex(4, 5);
+    p5.vertex(0, 1);
+    p5.vertex(-4, 5);
+    p5.endShape(p5.CLOSE);
+  };
 };
 
-new P5(sketch);
+new P5((p5) => sketch(p5, 400, 400));
