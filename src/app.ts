2125a6ec9480d07c3711cfef6f3507d62b79fc93
George Gaskin <george_gaskin@msn.com>
2021-07-04 23:02:08 +0200

    Backup

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,12 @@ import P5 from "p5";
 import { AsteroidsGame } from "./asteroidsGame";
 
 // Creating the sketch itself
-export const sketch = (p5: P5, setupCallBack:(p5:P5)=>void, drawCallBack:(p5:P5)=>void) => {
+export const sketch = (p5: P5, setupCallBack:(p5:P5)=>void) => {
   // The sketch setup method
   p5.setup = () => {
     setupCallBack(p5);
   };
 
-  // The sketch draw method
-  p5.draw = () => {
-    drawCallBack(p5);
-  
-  };
 };
 
 new AsteroidsGame()
--- src/asteroidsGame.ts
+++ src/asteroidsGame.ts
@@ -6,23 +6,30 @@ export class AsteroidsGame {
   spaceShip: Spaceship;
 
   constructor() {
-    new P5((p) => sketch(p, this.setup, this.gameLoop));
+    new P5((p) => sketch(p, this.setup));
   }
 
   public setup=(p5: P5)=> {
+    p5.keyPressed=()=>this.keyPressed(p5);
+    p5.draw=()=>this.gameLoop(p5);
     // Creating and positioning the canvas
     const canvas = p5.createCanvas(p5.windowWidth*0.8, p5.windowHeight*0.8);
     canvas.parent("app");
 
     // Configuring the canvas
-    p5.background("black");
     this.spaceShip = new Spaceship();
     this.spaceShip.setPosition(p5.createVector(p5.width / 2, p5.height / 2))
     this.spaceShip.setScale(2);
     this.spaceShip.setRotation(p5.PI)
   }
 
+  public keyPressed=(p5:P5)=> {   
+      if(p5.keyCode==p5.RIGHT_ARROW)
+        console.log('right');
+  }
+
   public gameLoop=(p5: P5)=> {
+    p5.background(0);
     this.spaceShip.update(p5);
     p5.noFill();
     p5.stroke('white');
